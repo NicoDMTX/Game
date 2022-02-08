47502d6b64a74f931fb713732a39a1381a32cacb
DEMONTOUX Nicolas <nicolas.demontoux@hotmail.com>
2022-02-08 19:48:38 +0100

    Configuration of the front environment

--- server.js
+++ server.js
@@ -1,34 +1,39 @@
-var express = require('express');
+const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path')
+const bodyParser = require('body-parser')
 const Character = require('./character')
 
+const app = express();
+const PORT = 3000;
+
+// Configuration of db
 mongoose.connect('mongodb://localhost/game', () => {
     console.log('connected');
 })
 
-
-async function createCharacter() {
-    try {
-        const character = new Character({ name : "Mage", level: 1 })
-        await character.save().then(() => console.log("Character saved"));
-        console.log(character);  
-    } catch (err) {
-        console.log(err);
-    }
-      
-}
-
-async function findCharacter() {
-    const character = await Character.findOne({})
-    console.log(character);
-}
-
-createCharacter()
-
-var app = express();
+express.static(path.join(__dirname, '/public'));
 
 app.get('/', (req,res) => {
-    res.send('test')
+    res.sendFile(__dirname + "/public/index.html");
 })
 
-app.listen(27017);
+app.post('/character', async (req,res) => {
+    const name = req.body.name;
+    const level = 1;
+
+    if(!name || !level) {
+        res.send('il manque un argument');
+        return;
+    }
+
+    const newCharacter =new Character({
+        name: name,
+        level : level
+    })
+    await newCharacter.save();
+    res.json(newCharacter);
+    return
+})
+
+app.listen(PORT, console.log(`Le port écoute au numéro ${PORT}`));
