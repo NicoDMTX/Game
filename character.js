7c21b18d61f8d18054ce4af0586cca0744476493
DEMONTOUX Nicolas <nicolas.demontoux@hotmail.com>
2022-02-13 14:53:26 +0100

    Improve the schema of the character

--- character.js
+++ character.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 
 const characterSchema = new mongoose.Schema({
     name: { type: String, unique: true },
-    level: Number
+    life: Number,
+    force: Number,
+    magie: Number,
+    dexterite: Number,
+    level: Number,
+    creationDate: { type: Date, default: Date.now },
 })
 
 module.exports = mongoose.model("Character", characterSchema)
